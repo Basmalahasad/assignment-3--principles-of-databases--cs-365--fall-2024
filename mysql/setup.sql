f9feeb747483d59796de3e23bffe31241147c8fb
Basmalah Asad <asad@hartford.edu>
2024-11-10 14:48:16 -0500

    Set up AES_ENCRYPT requirements

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -6,3 +6,7 @@ CREATE USER 'passwords_user'@'localhost' IDENTIFIED BY '';
 GRANT ALL ON student_passwords.* TO 'passwords_user'@'localhost';
 
 USE student_passwords;
+
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('assignment 3', 256));
+SET @init_vector = RANDOM_BYTES(16);
