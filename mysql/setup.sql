25b79808ad0753751c12ef554e0062829af7a64c
Basmalah Asad <asad@hartford.edu>
2024-11-10 14:41:44 -0500

    Create 'passwords_user' with no password required

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,3 +1,6 @@
 DROP DATABASE IF EXISTS student_passwords;
-
 CREATE DATABASE student_passwords;
+
+DROP USER IF EXISTS 'passwords_user'@'localhost';
+CREATE USER 'passwords_user'@'localhost' IDENTIFIED BY '';
+GRANT ALL ON student_passwords.* TO 'passwords_user'@'localhost';
