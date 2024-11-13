41d919705d4478e557bdabc540758c02d686de37
Basmalah Asad <asad@hartford.edu>
2024-11-13 14:17:52 -0500

    Set the key string and init vector as static

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,6 @@
 const DBNAME = "student_passwords";
 const DBHOST = "localhost";
 const DBUSER = "passwords_user";
-const DBPASS = "";
+const BLOCK_ENCRYPTION_MODE = "aes-256-cbc";
+const KEY_STR = "A1b2C3d4E5f6G7h8";
+const INIT_VECTOR = "1234ABCD5678EFGH";
